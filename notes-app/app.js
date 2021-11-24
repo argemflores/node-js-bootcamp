b89a7da0f924957046d1bc89126a7136acd8a612
Argem Flores <argemflores@gmail.com>
2021-11-24 21:30:11 +0800

    Create commands using yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,18 +1,53 @@
 // import modules and other files
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-// store command
-const command = process.argv[2]
+// customize yargs version
+yargs.version('1.1.0')
 
-// do command
-if (command === 'add') {
-    // add note
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    // remove note
-    console.log('Removing note!')
-}
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
 
-// print arguments
-console.log(process.argv)
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note!')
+    }
+})
+
+//
+// Challenge: Add two new commands
+//
+// 1. Set up command to support "list" command (print placeholder message for now)
+// 2. Set up command to support "read" command (print placeholder message for now)
+// 3. Test your work by running both commands and ensure correct output
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        console.log('Listing notes!')
+    }
+})
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note!')
+    }
+})
+
+// print yargs arguments
+console.log(yargs.argv)
